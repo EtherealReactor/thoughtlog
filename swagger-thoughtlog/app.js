6a72329edf56ca47ad61fbc957fa041fe295432b
RamPrasadReddy Maddi <ram.prasad@introhive.com>
2017-08-07 18:06:20 +0530

    Thoughtlog: User authentication system
    * Included signup, signin, profile and logout endpoints.

--- swagger-thoughtlog/app.js
+++ swagger-thoughtlog/app.js
@@ -2,16 +2,54 @@
 
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+// All the variables defined in some.env file will be acccessible as proces.env.SOME_VARIABLE
+// Please put any configuration variables in configuration.env.sample
+var dotenv = require('dotenv').config({ path: 'configuration.env' });
+var mongoose = require('mongoose');
+var UAParser = require('ua-parser-js');
+var { User } = require('./api/models/user_model');
+
 module.exports = app; // for testing
 
 var config = {
-  appRoot: __dirname // required config
+  appRoot: __dirname, // required config
+  swaggerSecurityHandlers: {
+    Bearer: function (req, authOrSecDef, token, callback) {
+      if (token) {
+        User.findByToken(token)
+          .then((user) => {
+            console.log(user);
+            if(!user) {
+              var err = new Error('No User found with the given token');
+              err['statusCode'] = 404;
+              callback(err);
+            }
+            req.swagger.params.auth_payload = user._id;
+            callback();
+          }).catch(() => {
+            var err = new Error('Failed to authenticate using bearer token');
+            err['statusCode'] = 401;
+            callback(err);
+          })
+      } else {
+        var err = new Error('Failed to authenticate using bearer token');
+        err['statusCode'] = 401;
+        callback(err);
+      }
+    }
+  }
 };
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
 
-  // install middleware
+  if (err) { throw err; }
+  // Mongoose connection, promises and error handling
+  mongoose.connect(process.env.DATABASE, { useMongoClient: true }); // connection for mongodb
+  mongoose.Promise = global.Promise; // Sticking with ES6 promises only
+  mongoose.connection.on('error', (err) => {
+    console.error(`🚫  🚫  🚫  🚫 ➡ ${err.message}`);
+  });
+
   swaggerExpress.register(app);
 
   var port = process.env.PORT || 10010;
@@ -20,4 +58,5 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (swaggerExpress.runner.swagger.paths['/hello']) {
     console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
   }
+
 });
